d5ced7374a2dc891c07d06e588267eea52e9b768
callum <cjoseph@aiudo.es>
2022-10-03 17:57:44 +0200

    now with the arrays

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
 
 
-let sales: number = 123_456_789;
+let sales = 123_456_789;
 
 let course: string = 'TypeScript';
 
 let is_published: boolean = true;
+
+let level;
+
+
+let numbers = [1, 2, 3, '4', 5];
