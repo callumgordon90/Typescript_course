4bb99d90ad32b91dda301dfc21aa9f7fc48c40af
callum <cjoseph@aiudo.es>
2022-10-04 10:47:30 +0200

    fixed some bugs

--- dist/index.js
+++ dist/index.js
@@ -4,10 +4,26 @@ let course = 'TypeScript';
 let is_published = true;
 let level;
 // arrays
-let numbers = [1, 2, 3, '4', 5];
-numbers.forEach(n => n.);
+// let numbers: number[] = [1, 2, 3, '4', 5];
+// numbers.forEach(n => n.)
 //tuples
 // 1, 'Callum'
 let user = [1, 'Callum'];
 let man = 'louis';
+//enums
+//enums represent a list of related constants. for example imagine we want to define the sizes of t shirts as constants:
+const small = 1;
+const medium = 2;
+const large = 3;
+// one way to list these is to group them together like so:
+//PascalCase     ... and by default the typescript compliler assigns the first member the value of zero, although there are other ways to assign values too.
+var Size;
+(function (Size) {
+    Size[Size["Small"] = 0] = "Small";
+    Size[Size["Medium"] = 1] = "Medium";
+    Size[Size["Large"] = 2] = "Large";
+})(Size || (Size = {}));
+;
+let mySize = Size.Medium;
+console.log(mySize);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -10,9 +10,9 @@ let level;
 
 // arrays
 
-let numbers: number[] = [1, 2, 3, '4', 5];
+// let numbers: number[] = [1, 2, 3, '4', 5];
 
-numbers.forEach(n => n.)
+// numbers.forEach(n => n.)
 
 //tuples
 
@@ -22,3 +22,20 @@ let user: [number, string] = [1, 'Callum'];
 
 
 let man = 'louis';
+
+//enums
+
+//enums represent a list of related constants. for example imagine we want to define the sizes of t shirts as constants:
+
+const small = 1;
+const medium = 2;
+const large = 3;
+
+// one way to list these is to group them together like so:
+
+//PascalCase     ... and by default the typescript compliler assigns the first member the value of zero, although there are other ways to assign values too.
+enum Size { Small = 0, Medium = 1, Large = 2 };
+
+let mySize: Size = Size.Medium;
+
+console.log(mySize);
